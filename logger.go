114767f7dca372ee6e9981752f97038e134d2a09
Martin Sucha <martin.sucha@kiwi.com>
2021-04-12 18:16:31 +0200

    Add per-session logger
    There are race conditions in tests that swap the global logger,
    we shouldn't modify any global state.

--- logger.go
+++ logger.go
@@ -27,4 +27,6 @@ func (l *defaultLogger) Print(v ...interface{})                 { log.Print(v...
 func (l *defaultLogger) Printf(format string, v ...interface{}) { log.Printf(format, v...) }
 func (l *defaultLogger) Println(v ...interface{})               { log.Println(v...) }
 
+// Logger for logging messages.
+// Deprecated: Use ClusterConfig.Logger instead.
 var Logger StdLogger = &defaultLogger{}
